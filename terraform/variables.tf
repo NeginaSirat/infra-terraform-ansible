ac7ebb8b650de20073dbac0bed0e834d91b59fd7
Negina Sirat <129796223+NeginaSirat@users.noreply.github.com>
2025-11-15 17:55:21 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,28 @@
 variable "aws_region" {
-  type    = string
-  default = "us-west-2"
+  default = "us-east-1"
 }
-variable "vpc_cidr" { type = string, default = "10.0.0.0/16" }
-variable "subnet_cidr" { type = string, default = "10.0.1.0/24" }
-variable "instance_type" { type = string, default = "t3.medium" }
-variable "key_name" { type = string, default = "demo-key" }
-variable "public_key_path" { type = string, default = "~/.ssh/id_rsa.pub" }
-variable "cluster_name" { type = string, default = "demo-eks" }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "project_name" {
+  default = "custom-vpc"
+}
+  
+  variable "instance_type" {
+  default = "m7i-flex.large"
+}
+
+variable "key_name" {
+  description = "jenkins-key"
+  type        = string
+}
