faeca43de5df349384de04bf03c9b2062430b1b7
Negina Sirat <129796223+NeginaSirat@users.noreply.github.com>
2025-11-15 17:54:58 -0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,27 @@
-output "app_public_ip" {
-  description = "Public IP of the demo EC2 instance"
-  value       = aws_instance.app.public_ip
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "security_group_id" {
+  value = aws_security_group.devops_sg.id
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "nexus_public_ip" {
+  value = aws_instance.nexus.public_ip
+}
+
+output "sonarqube_public_ip" {
+  value = aws_instance.sonarqube.public_ip
 }
